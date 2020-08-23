f35298857d770480350ea303bf38768b5457cd0b
Vinothak <56429360+Vinothak@users.noreply.github.com>
2020-08-24 01:25:36 +0530

    Added my pagination component

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
-const Pagination = () => {
+const Pagination = ({ issuesLimit, totalIssues, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalIssues / issuesLimit); i++) {
+    pageNumbers.push(i);
+  }
+  console.log(pageNumbers.length);
+
   return (
-    <div>
-      
-    </div>
+    <nav class='outer-page'>
+      <ul className='pagination'>
+        {pageNumbers.map(number => (
+          <li key={number} className='page-item'>
+            <a onClick={() => paginate(number)} href='!#' className='page-link'>
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
