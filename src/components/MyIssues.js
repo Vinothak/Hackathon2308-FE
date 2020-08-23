350292523f493f987e7d2bd0451a78abfe8a9064
Vinothak <56429360+Vinothak@users.noreply.github.com>
2020-08-24 01:30:01 +0530

    Added an import for mystyle.css

--- src/components/MyIssues.js
+++ src/components/MyIssues.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Mystyle.css'
 import InfoIcon from '@material-ui/icons/Info';
 import axios from 'axios';
 import Modal from 'react-modal'
