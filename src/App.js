bdd6e00afe0a0105f068413ca02d97df6d4e96cc
Vinothak <56429360+Vinothak@users.noreply.github.com>
2020-08-24 01:19:42 +0530

    added my whole app.js as a component to this original App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Issues from "./components/Issues";
+import MyIssues from "./components/MyIssues";
 function App() {
   return (
+    <>
     <Issues/>
+    <MyIssues/>
+    </>
   );
 }
 
